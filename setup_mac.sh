501a81db29922c672ee1622eb498c0534d165c5c
panchomb <fmagot01@gmail.com>
2023-12-13 01:14:01 +0100

    updated setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -5,7 +5,7 @@ pip3 install -r requirements.txt
 
 brew install espeak
 
-git clone https://github.com/Rudrabha/Wav2Lip.git
+git clone https://github.com/panchomb/Wav2Lip.git
 
 wget 'https://github.com/justinjohn0306/Wav2Lip/releases/download/models/wav2lip.pth' -O 'checkpoints/wav2lip.pth'
 wget 'https://github.com/justinjohn0306/Wav2Lip/releases/download/models/wav2lip_gan.pth' -O 'checkpoints/wav2lip_gan.pth'
